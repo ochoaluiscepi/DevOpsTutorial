eba7cc0a730ad7e723eb65b7639eb15de3c766e3
Luis Ochoa <ochoaluis_cepi@hotmail.com>
2021-12-29 11:36:15 -0600

    Initial

---
+++ DevOpsTutorial/DevOpsTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevOpsTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
